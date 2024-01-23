e338d885cd2f6f7a6afcd6a2b61521765f75422a
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-23 05:50:04 -0300

    Criação de métodos limpar e recuperar lista

--- Lista/ListaDeCompras.java
+++ Lista/ListaDeCompras.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class ListaDeCompras {
-
     private ArrayList<String> lista = new ArrayList<>();
+    private ArrayList<String> listaBackup = new ArrayList<>();
 
     public ListaDeCompras() {
         paginaInicial();
@@ -29,10 +29,10 @@ public class ListaDeCompras {
                     visualizarlista();
                     break;
                 case "5":
-                    lista.clear();
+                    limparLista();
                     break;
                 case "6":
-
+                    recuperarLista();
                     break;
                 case "7":
                     System.exit(0);
@@ -40,12 +40,11 @@ public class ListaDeCompras {
                 default:
                     opcaoEscolhida = false;
                     JOptionPane.showMessageDialog(null, "Nenhuma opção foi selecionada, tente novamente");
-                    opcao = JOptionPane.showInputDialog(null, "LISTA DE COMPRAS\n\n 1 - Adicionar item á lista \n 2 - Editar item da lista \n 3 - Excluir item da lista \n 4 - Visualizar itens da lista \n 5 - limpar itens da lista \n 6 - recuperar itens da lista  \n 7 - Fechar Sistema\n\n");
+                    opcao = JOptionPane.showInputDialog(null, "LISTA DE COMPRAS\n\n 1 - Adicionar item á lista \n 2 - Editar item da lista \n 3 - Excluir item da lista \n 4 - Visualizar itens da lista \n 5 - Limpar itens da lista \n 6 - Recuperar itens da lista  \n 7 - Fechar Sistema\n\n");
                     break;
             }
         } while (!opcaoEscolhida);
     }
-
     private void adicionarItem() {
 
     }
@@ -59,6 +58,14 @@ public class ListaDeCompras {
     }
 
     private void editarItem() {
+
+    }
+
+    private void limparLista() {
+
+    }
+
+    private void recuperarLista() {
 
     }
 }
