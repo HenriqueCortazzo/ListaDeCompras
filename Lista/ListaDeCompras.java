730503b7e49057fba310f625af0ec527f1a9778c
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-23 05:10:52 -0300

    Criação de métodos

--- Lista/ListaDeCompras.java
+++ Lista/ListaDeCompras.java
@@ -1,4 +1,30 @@
 package Lista;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class ListaDeCompras {
+
+    private ArrayList<String> lista = new ArrayList<>();
+
+
+    public ListaDeCompras(){
+
+    }
+    
+    private void adicionarItem() {
+
+    }
+
+    private void excluirItem() {
+
+    }
+
+    private void visualizarItem() {
+
+    }
+
+    private void editarItem() {
+
+    }
 }
--- Lista/Main.java
+++ Lista/Main.java
@@ -1,4 +1,8 @@
 package Lista;
 
 public class Main {
+    public static void main(String[] args) {
+        ListaDeCompras lista = new ListaDeCompras();
+
+    }
 }
