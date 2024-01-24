415f82b8d641b63323c0db523731f82478659565
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-24 12:11:28 -0300

    Inclusão novo modelo de formatação

--- Lista/ListaDeCompras.java
+++ Lista/ListaDeCompras.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class ListaDeCompras {
     private ArrayList<String> lista = new ArrayList<>();
+    private ArrayList<String> resultados = new ArrayList<>();
     private ArrayList<String> listaBackup = new ArrayList<>();
 
     private double valorProd;
@@ -58,17 +59,17 @@ public class ListaDeCompras {
                 valorResultante = 0;
                 qtd = 0;
             }
-                String nomeProduto = JOptionPane.showInputDialog(null, "Digite o nome do produto: ");
-                String qtdItem = JOptionPane.showInputDialog(null, "Quantidade: ");
-                String valorProduto = JOptionPane.showInputDialog(null, "Valor: ");
-                valorProd = Double.parseDouble(valorProduto);
-                qtd = Integer.parseInt(qtdItem);
-                valorResultante = qtd * valorProd;
-                if (!nomeProduto.equalsIgnoreCase("null") && !qtdItem.equalsIgnoreCase("null") && !valorProduto.equalsIgnoreCase("null")) {
-                    lista.add(nomeProduto);
-                    JOptionPane.showMessageDialog(null, "Produto listado com sucesso.");
-                    tudoOk = true;
-                }
+            String nomeProduto = JOptionPane.showInputDialog(null, "Digite o nome do produto: ");
+            String qtdItem = JOptionPane.showInputDialog(null, "Quantidade: ");
+            String valorProduto = JOptionPane.showInputDialog(null, "Valor: ");
+            valorProd = Double.parseDouble(valorProduto);
+            qtd = Integer.parseInt(qtdItem);
+            valorResultante = qtd * valorProd;
+            if (!nomeProduto.equalsIgnoreCase("null") && !qtdItem.equalsIgnoreCase("null") && !valorProduto.equalsIgnoreCase("null")) {
+                lista.add(nomeProduto);
+                JOptionPane.showMessageDialog(null, "Produto listado com sucesso.");
+                tudoOk = true;
+            }
 
         } while (!tudoOk);
         paginaInicial();
@@ -98,13 +99,14 @@ public class ListaDeCompras {
 
     private void visualizarlista() {
         String resultado = "PRODUTO   QNTD   VALOR UN   VALOR TOTAL\n";
-        double valorUnExibir = valorProd;
-        int quant = qtd;
-        double valorTotalExibir = valorUnExibir * quant;
         for (String produto : lista) {
-            resultado += String.format("%-18s   %-9d   %-16.2f   %-15.2f\n", produto, quant, valorUnExibir, valorTotalExibir);
+            resultado += String.format("%-18s   %-9d   %-16.2f   %-15.2f\n", produto, qtd, valorProd, valorResultante);
+        }
+        if (!resultados.isEmpty()) {
+            JOptionPane.showMessageDialog(null, resultados);
         }
         JOptionPane.showMessageDialog(null, resultado);
+        resultados.add(resultado);
         paginaInicial();
     }
 
